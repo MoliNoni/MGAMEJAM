1de3363064476b9e28c51eca8dc6a1469492aa2c
Juan José Molina Peña <nonibrownie@gmail.com>
2025-11-24 06:40:48 -0500

    feat: Normalization toggle logic update
    Now normalization contains a timer that will restrict the mechanic's usage
    
    + Normalization timer added
    + AddTimeToTimer method added
    + Normalization is already toggleable and directly linked with timer
    + FPS Limiter script added

--- Assets/Player Assets/Scripts/Normalization.cs
+++ Assets/Player Assets/Scripts/Normalization.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Normalization : MonoBehaviour
 {
+    [SerializeField] private bool isNormalizationOn = false;
+     [SerializeField] private float timerCurrentTime = 5f;
+     [SerializeField] public float maxTimerDuration =6f;
     private GameObject[] objs;
 
     void Start()
@@ -13,13 +16,59 @@ public class Normalization : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+
+
+        if (Input.GetKeyDown(KeyCode.R) && isNormalizationOn == false && timerCurrentTime > 0.5f)
         {
-            foreach (GameObject obj in objs)
+            Normalize();
+            isNormalizationOn = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.R) && isNormalizationOn == true)
+        {
+            Normalize();
+            isNormalizationOn = false;
+        }
+
+        if (isNormalizationOn == true )
+        {
+            timerCurrentTime = timerCurrentTime-Time.deltaTime;
+
+            if (timerCurrentTime<=0)    //In order to interrumpt Normalization in case it runs out of time
             {
-                bool state = obj.activeSelf;
-                obj.SetActive(!state);
+                Normalize();
+                isNormalizationOn = false;
             }
         }
+        
+
+}
+    void Normalize()
+    {
+        foreach (GameObject obj in objs)
+        {
+            obj.SetActive(!obj.activeSelf);
+        }
+    }
+
+    void ResumeTimer()
+    {
+        
+    }
+
+    void PauseTimer()
+    {
+        
+    }
+
+    void AddTimeToTimer(float seconds)
+    {
+        timerCurrentTime += seconds;
+        if (timerCurrentTime > maxTimerDuration)
+        {
+            timerCurrentTime = maxTimerDuration;
+        }
+        
     }
 }
+
+
